b0586dbee896230d3661c6ca9a88cffd70794332
Portal Lusitano <portal.lusitano2023@gmail.com>
2026-01-15 16:22:39 +0000

    3

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Ignora erros de lint e typescript para o build passar
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Permite carregar as fotos do Sanity e Shopify
   images: {
     remotePatterns: [
       {
