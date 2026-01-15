a057960006b4f95f31cc7fe4ac822726f300d051
Portal Lusitano <portal.lusitano2023@gmail.com>
2026-01-15 21:54:16 +0000

    3

--- lib/client.ts
+++ lib/client.ts
@@ -1,10 +1,16 @@
+// @ts-nocheck
 import { createClient } from 'next-sanity'
-
 import { apiVersion, dataset, projectId } from '../sanity/env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  // 1. ALTERAÇÃO CRÍTICA: useCdn tem de ser false para permitir escritas (licitações)
+  // e para que o Dashboard mostre os dados no segundo exato em que chegam.
+  useCdn: false, 
+  
+  // 2. ADIÇÃO CRÍTICA: O token permite que este cliente "escreva" no teu Sanity.
+  // Ele vai ler a variável SANITY_API_WRITE_TOKEN que configuraste no Vercel.
+  token: process.env.SANITY_API_WRITE_TOKEN,
 })
