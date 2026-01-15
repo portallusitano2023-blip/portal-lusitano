dc3bc9198f33fc2a624261df3e6486cae141877d
Portal Lusitano <portal.lusitano2023@gmail.com>
2026-01-15 16:17:48 +0000

    2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,23 @@
 const nextConfig = {
+  // Ignorar erros na construção
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  // === LISTA VIP DE IMAGENS ===
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io', // Imagens dos Cavalos
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com', // Imagens da Loja de Roupa
+      }
+    ],
   },
 };
 
